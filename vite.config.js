d735e9a980b9fd73efcdba14bcc53568701cad12
Mathesh <dummy@gmail.com>
2025-05-19 17:04:35 +0530

    Added categories

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/React-onedrive-app/",
+  base: "/React-onedrive-app/", // repository name
   plugins: [react()],
 });
